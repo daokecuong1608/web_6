f1f66f767ac7af06bac61025319253c858bb8c11
unknown <tqmminh2004tqm@gmail.com>
2025-04-18 07:50:24 +0700

    update

--- index.php
+++ index.php
@@ -26,104 +26,103 @@ if ($conn->connect_error) {
 <body>
 
     <?php
-        include 'header.php';
-        include 'carousel.php';
+    include 'header.php';
+    include 'carousel.php';
     ?>
 
-    <?php  
+    <?php
     //Xác định trang
-    if(isset($_GET['trang'] )){
+    if (isset($_GET['trang'])) {
         $page = $_GET['trang'];
-    }else{
-        $page= 1;  
+    } else {
+        $page = 1;
     }
     //Xác định vị trí bắt đầu
-    if($page == '' || $page == 1){
+    if ($page == '' || $page == 1) {
         $begin = 0;
-
-    }else{
-            $begin = ($page * 8) - 8;
-        }
-        //truy vấn 
+    } else {
+        $begin = ($page * 8) - 8;
+    }
+    //truy vấn 
     $sql = "SELECT * FROM tbl_sanpham, tbl_loaisanpham 
             WHERE tbl_sanpham.loaisanpham_id = tbl_loaisanpham.loaisanpham_id 
             ORDER BY sanpham_id DESC 
-            LIMIT $begin, 8";  
-          $query = mysqli_query($conn,$sql);
-       ?>
+            LIMIT $begin, 8";
+    $query = mysqli_query($conn, $sql);
+    ?>
     <div class="container mt-5">
         <h1 class="text-center">Sản phẩm mới</h1>
         <hr>
         <div class="row">
             <?php
-                while($row = mysqli_fetch_array($query)){
+            while ($row = mysqli_fetch_array($query)) {
             ?>
-            <div class="col-md-3 mb-3">
-                <div class="card">
-                    <img src="images/product/<?php echo $row['sanpham_anh'] ?>" class="card-img-top"
-                        alt="<?php echo $row['sanpham_tieude'] ?>">
-                    <div class="card-body text-center">
-                        <h5 class="card-title"><?php echo $row['sanpham_tieude'] ?></h5>
-                        <p class="card-text price">Giá bán:
-                            <?php echo number_format($row['sanpham_gia'], 0, ',', '.') . ' VNĐ' ?></p>
-                        <a href="mua_ngay.php?sanpham_id=<?php echo $row['sanpham_id']; ?>" class="btn btn-info">Mua
-                            ngay</a>
-                        <a href="view_product.php?sanpham_id=<?php echo $row['sanpham_id']; ?>"
-                            class="btn btn-secondary">Xem
-                            nhanh</a>
+                <div class="col-md-3 mb-3">
+                    <div class="card">
+                        <img src="images/product/<?php echo $row['sanpham_anh'] ?>" class="card-img-top"
+                            alt="<?php echo $row['sanpham_tieude'] ?>">
+                        <div class="card-body text-center">
+                            <h5 class="card-title"><?php echo $row['sanpham_tieude'] ?></h5>
+                            <p class="card-text price">Giá bán:
+                                <?php echo number_format($row['sanpham_gia'], 0, ',', '.') . ' VNĐ' ?></p>
+                            <a href="mua_ngay.php?sanpham_id=<?php echo $row['sanpham_id']; ?>" class="btn btn-info">Mua
+                                ngay</a>
+                            <a href="view_product.php?sanpham_id=<?php echo $row['sanpham_id']; ?>"
+                                class="btn btn-secondary">Xem
+                                nhanh</a>
+                        </div>
                     </div>
                 </div>
-            </div>
             <?php
             }
-        ?>
+            ?>
         </div>
     </div>
 
     <div class="pagination-container">
         <?php
-            $sql_2 = "select * from tbl_sanpham";
-            $query_2 = mysqli_query($conn,$sql_2);
-             $count = mysqli_num_rows($query_2);
-              $a = ceil($count/8);
-            ?>
+        $sql_2 = "select * from tbl_sanpham";
+        $query_2 = mysqli_query($conn, $sql_2);
+        $count = mysqli_num_rows($query_2);
+        $a = ceil($count / 8);
+        ?>
         <ul class="pagination">
             <?php
-                 if($page > 1){
-                    //?trang là tham số truyền qua
-                ?>
-            <li class="page-item">
-                <a class="page-link" href="?trang=<?php echo $page-1; ?>">Previous</a>
-            </li>
+            if ($page > 1) {
+                //?trang là tham số truyền qua
+            ?>
+                <li class="page-item">
+                    <a class="page-link" href="?trang=<?php echo $page - 1; ?>">Previous</a>
+                </li>
             <?php
-                            }
-                  for($b=1;$b<=$a;$b++){
-                 if($page == $b){
+            }
+            for ($b = 1; $b <= $a; $b++) {
+                if ($page == $b) {
                     $active = "active";
-                      }else{
+                } else {
                     $active = "";
-                   }
-                            ?>
-            <li class="page-item <?php echo $active ?>">
-                <a class="page-link" href="?trang=<?php echo $b ?>"><?php echo $b ?></a>
-            </li>
+                }
+            ?>
+                <li class="page-item <?php echo $active ?>">
+                    <a class="page-link" href="?trang=<?php echo $b ?>"><?php echo $b ?></a>
+                </li>
 
             <?php
-                }
-                if($page < $a){
-                ?>
-            <li class="page-item">
-                <a class="page-link" href="?trang=<?php echo $page+1; ?>">Next</a>
-            </li>
+            }
+            if ($page < $a) {
+            ?>
+                <li class="page-item">
+                    <a class="page-link" href="?trang=<?php echo $page + 1; ?>">Next</a>
+                </li>
             <?php
-                }
-                ?>
+            }
+            ?>
         </ul>
 
     </div>
 
     <?php
-       include 'footer.php';
+    include 'footer.php';
     ?>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
@@ -132,6 +131,15 @@ if ($conn->connect_error) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"
         integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous">
     </script>
+
+    <!-- Chatbot dialogflow -->
+    <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+    <df-messenger
+        intent="WELCOME"
+        chat-title="Chat_Shop_AI"
+        agent-id="ab7a00d3-d7f8-41bb-8af3-9696ffdf2815"
+        language-code="vi">
+    </df-messenger>
 
 </body>
 
